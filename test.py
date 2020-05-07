62a31c484d63fcfd50dd681ff5c352b10e5a75a8
long-link <evgeny76yar@gmail.com>
2020-05-08 00:17:06 +0300

    version 1.0. Cyrillic don't work.

---
+++ test.py
@@ -0,0 +1,54 @@
+import struct
+import os
+# a = 5
+# b = 2
+# for a in range(4):
+#     b += a
+#     print(b)
+# b = [0, 4, 5, 7]
+# a = "qwe"
+# c = a[:-2]
+# a = a[2:]
+# print(c)
+# a = a.encode('utf-8')
+
+# c = '9'
+# a = 'asd'
+# binary_message = bin(int.from_bytes(c.encode('utf-8'), byteorder='big'))
+# print(binary_message)
+c = b'a'
+print(c.decode('utf-8'))
+# a = int.from_bytes(c, byteorder='big')
+# a = bin(a)
+#
+# print(a)
+#
+b = '01100001'
+a = int(b, 2).to_bytes(1, byteorder='big')
+print(a)
+
+# b = 0b10
+# a = b.to_bytes(1, byteorder='big')
+# print(a)
+# e = 0b0 + 10
+# print(struct.pack(">b", b))
+# 0b110001
+
+# array_bytes = []
+# with open('image.gif', 'rb') as file:
+#     while True:
+#         byte = file.read(1)
+#         if byte:
+#             array_bytes.append(byte)
+#         else:
+#             break
+#
+# file2 = "popka"
+#
+# with open(os.path.join("C:\\Users\\evgen\\PycharmProjects\\steganographyGIF", file2 + ".gif"), "wb") as f:
+#     # f.write(chr(array_bytes))
+#     #f = "".join([chr(i) for i in array_bytes])
+#     #f.append([int(i) for i in array_bytes])
+#     for i in array_bytes:
+#         f.write(i)
+
