90a0d79005c595b2eb4841d86d1fac76ba3f858a
long-link <evgeny76yar@gmail.com>
2020-05-08 21:08:01 +0300

    version 2.0 Interface combined two classes into one file metodLSB. mine brings it all together

--- test.py
+++ test.py
@@ -16,16 +16,16 @@ import os
 # a = 'asd'
 # binary_message = bin(int.from_bytes(c.encode('utf-8'), byteorder='big'))
 # print(binary_message)
-c = b'a'
-print(c.decode('utf-8'))
+# c = b'a'
+# print(c.decode('utf-8'))
 # a = int.from_bytes(c, byteorder='big')
 # a = bin(a)
 #
 # print(a)
 #
-b = '01100001'
-a = int(b, 2).to_bytes(1, byteorder='big')
-print(a)
+# b = '01100001'
+# a = int(b, 2).to_bytes(1, byteorder='big')
+# print(a)
 
 # b = 0b10
 # a = b.to_bytes(1, byteorder='big')
@@ -52,3 +52,40 @@ print(a)
 #     for i in array_bytes:
 #         f.write(i)
 
+# def pisya(a):
+#     if a == 2:
+#         return True
+#     return 5
+#
+# qw = 3
+# b = pisya(qw)
+# if not b:
+#     print(b)
+# else:
+#     print(b)
+#
+# errors = {
+#     2: 'qwe',
+#     4: 'asd',
+#     5: 'zxc',
+#     8: 'rty',
+#     9: 'fgh'
+# }
+#
+# print(errors[b])
+
+def read_bytes(file_name):
+    try:
+        array_bytes = []
+        with open(file_name, 'rb') as file:
+            while True:
+                byte = file.read(1)
+                if byte:
+                    array_bytes.append(byte)
+                else:
+                    break
+        return array_bytes
+    except FileNotFoundError:
+        pass
+
+print(read_bytes(''))
